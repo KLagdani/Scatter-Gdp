c9f492878151466594239dbc5961a27043c0bebb
Kaoutar <lagdani.kaoutar@gmail.com>
2020-04-10 17:43:37 +0200

    UPD transition

--- src/components/Gdp.js
+++ src/components/Gdp.js
@@ -15,62 +15,59 @@ export class Gdp extends Component {
   constructor() {
     super();
     this.state = {
-      margins: {
-        left: 100,
-        top: 100,
-        right: 100,
-        bottom: 100
-      },
-      width: 700,
-      height: 400,
-      incomeScale: "",
+      g: "",
       lifeExpecScale: "",
+      incomeScale: "",
       populationScale: "",
-      xAxisGroup: "",
-      yAxisGroup: "",
-      t: d3.transition().duration(75),
-      yearCount: 0,
-      yearGroup: ""
+      yearGroup: "",
+      continentScale: "",
+      count: 0
     };
+    this.updateData = this.updateData.bind(this);
   }
 
   componentDidMount() {
-    const { margins, width, height } = this.state;
+    var margin = { left: 100, right: 100, top: 100, bottom: 100 };
+    var height = 500 - margin.top - margin.bottom,
+      width = 800 - margin.left - margin.right;
 
-    //SVG Area
-    const g = d3
+    var g = d3
       .select(this.refs.chartArea)
       .append("svg")
-      .attr("width", width + margins.left + margins.right)
-      .attr("height", height + margins.top + margins.bottom)
+      .attr("width", width + margin.left + margin.right)
+      .attr("height", height + margin.top + margin.bottom)
       .append("g")
-      .attr("transform", `translate(${margins.left}, ${margins.top})`);
+      .attr("transform", `translate(${margin.left}, ${margin.top})`);
 
-    //LABELS
-    g.append("text")
-      .attr("class", "x axis-label")
-      .attr("x", width / 2)
+    // Labels
+    var xLabel = g
+      .append("text")
       .attr("y", height + 50)
+      .attr("x", width / 2)
       .attr("font-size", "20px")
       .attr("text-anchor", "middle")
-      .text("GDP Per Capita");
-    g.append("text")
-      .attr("class", "y axis-label")
-      .attr("x", -(height / 2))
-      .attr("y", -40)
-      .attr("font-size", "20px")
-      .attr("text-anchor", "middle")
+      .text("GDP Per Capita ($)");
+    var yLabel = g
+      .append("text")
       .attr("transform", "rotate(-90)")
-      .text("Life expectancy");
-    const yearGroup = g
+      .attr("y", -40)
+      .attr("x", -170)
+      .attr("font-size", "20px")
+      .attr("text-anchor", "middle")
+      .text("Life Expectancy (Years)");
+    var yearGroup = g
       .append("text")
       .attr("class", "year-label")
-      .attr("x", width - 100)
-      .attr("y", 40)
+      .attr("x", width - 50)
+      .attr("y", height - 20)
       .attr("font-size", "20px")
       .attr("text-anchor", "middle")
       .text("Year");
-    this.setState({ yearGroup });
+
+    this.setState({
+      g,
+      yearGroup
+    });
 
     //DATA
     d3.json("data/gdp.json")
@@ -94,24 +91,37 @@ export class Gdp extends Component {
           };
         });
 
-        //Scales
-        const incomeScale = d3
+        // Scales
+        var incomeScale = d3
           .scaleLog()
-          .domain([minIncome(data), maxIncome(data)])
+          .base(10)
           .range([0, width])
-          .base(10);
-
-        const lifeExpecScale = d3
+          .domain([minIncome(data), maxIncome(data)]);
+        var lifeExpecScale = d3
           .scaleLinear()
-          .domain([0, maxLifeExpec(data)])
-          .range([height, 0]);
-
-        const populationScale = d3
+          .range([height, 0])
+          .domain([0, maxLifeExpec(data)]);
+        var populationScale = d3
           .scaleLinear()
-          .domain([minPopulation(data), maxPopulation(data)])
-          .range([5, 60]);
+          .range([5, 60])
+          .domain([minPopulation(data), maxPopulation(data)]);
+        var continentScale = d3.scaleOrdinal(d3.schemePastel1);
 
-        const continentScale = d3.scaleOrdinal(d3.schemePastel1);
+        // X Axis
+        var xAxisCall = d3
+          .axisBottom(incomeScale)
+          .tickValues([500, 5000, 50000])
+          .tickFormat(d3.format("$"));
+        g.append("g")
+          .attr("class", "x axis")
+          .attr("transform", "translate(0," + height + ")")
+          .call(xAxisCall);
+
+        // Y Axis
+        var yAxisCall = d3.axisLeft(lifeExpecScale).tickFormat(d => +d);
+        g.append("g")
+          .attr("class", "y axis")
+          .call(yAxisCall);
 
         this.setState({
           incomeScale,
@@ -120,73 +130,56 @@ export class Gdp extends Component {
           continentScale
         });
 
-        //X Axis
-        const xAxisCall = d3
-          .axisBottom(incomeScale)
-          .tickValues([500, 5000, 50000])
-          .tickFormat(d3.format("$"));
-        const xAxisGroup = g
-          .append("g")
-          .attr("class", "x-axis")
-          .attr("transform", `translate(0, ${height})`)
-          .call(xAxisCall);
-
-        //Y Axis
-        const yAxisCall = d3.axisLeft(lifeExpecScale);
-        const yAxisGroup = g
-          .append("g")
-          .attr("class", "y-axis")
-          .call(yAxisCall);
-
-        this.setState({ xAxisGroup, yAxisGroup });
-
-        //Intervals
         d3.interval(() => {
           this.setState({
-            yearCount:
-              this.state.yearCount > data.length - 2
-                ? 0
-                : this.state.yearCount + 1
+            count: this.state.count < data.length - 2 ? this.state.count + 1 : 0
           });
-          this.updateData(g, cleanData[this.state.yearCount]);
-        }, 1000);
-        this.updateData(g, cleanData[0]);
+          this.updateData(cleanData[this.state.count]);
+        }, 100);
 
+        this.updateData(cleanData[0]);
         this.buildLegend(data);
       })
+
       .catch(err => console.log(err));
   }
 
-  updateData(g, data) {
-    let {
-      incomeScale,
-      lifeExpecScale,
-      populationScale,
+  updateData(data) {
+    var {
+      g,
       continentScale,
-      height,
+      lifeExpecScale,
+      incomeScale,
+      populationScale,
       yearGroup,
-      t
+      count
     } = this.state;
 
-    //JOIN new data with old elements
-    const circles = g.selectAll("circle").data(data.countries);
+    var t = d3.transition().duration(100);
 
-    //EXIT old elements not present in new data
-    circles.exit().remove();
+    // JOIN new data with old elements.
+    var circles = g.selectAll("circle").data(data.countries, d => {
+      return d.country;
+    });
+
+    // EXIT old elements.
+    circles
+      .exit()
+      .attr("class", "exit")
+      .remove();
 
     //ENTER new elements and Drawing circles
     circles
       .enter()
       .append("circle")
-      .attr("cx", d => incomeScale(d.income))
-      .attr("cy", d => height - lifeExpecScale(d.life_exp))
-      .attr("r", populationScale(0))
-      .attr("fill", d => continentScale(d.continent))
-      .attr("opacity", "0.8")
+      .attr("class", "enter")
+      .attr("fill", d => {
+        return continentScale(d.continent);
+      })
       .merge(circles)
       .transition(t)
+      .attr("cy", d => lifeExpecScale(d.life_exp))
       .attr("cx", d => incomeScale(d.income))
-      .attr("cy", d => height - lifeExpecScale(d.life_exp))
       .attr("r", d => populationScale(d.population));
 
     yearGroup.text(`Year ${data.year}`);
@@ -200,10 +193,9 @@ export class Gdp extends Component {
       .append("svg")
       .attr("class", "chart-legend-svg")
       .attr("width", "1000")
-      .attr("height", "500");
+      .attr("height", "300");
 
     //Continent Legend
-
     const continentG = svg.append("g").attr("transform", `translate(100, 90)`);
     const continentCircles = continentG
       .selectAll("circle")
@@ -233,37 +225,6 @@ export class Gdp extends Component {
         var x = i * 200 - legendContinentText / 2;
         return "translate(" + [x, 48] + `)`;
       });
-
-    //Population Legend
-    // const g = svg.append("g").attr("transform", `translate(100, 210)`);
-    // const circles = g.selectAll("circle").data(populationData(data));
-    // const circlesG = circles.enter().append("g");
-    // circlesG
-    //   .append("circle")
-    //   .attr("class", (d, i) => `legend-population legend-population-${i}`)
-    //   .attr("cx", (d, i) => i * 200)
-    //   .attr("cy", 10)
-    //   .attr("r", d => populationScale(d))
-    //   .attr("fill", "none")
-    //   .attr("stroke", "grey")
-    //   .attr("stroke-width", "1")
-    //   .attr("opacity", "0.8");
-
-    // circlesG
-    //   .append("text")
-    //   .text(d => d)
-    //   .attr(
-    //     "class",
-    //     (d, i) => `paragraph legend-population legend-population-text-${i}`
-    //   )
-    //   .attr("transform", (d, i) => {
-    //     var legendPopulationText = d3
-    //       .select(`.legend-population-text-${i}`)
-    //       .node()
-    //       .getBoundingClientRect().width;
-    //     var x = i * 200 - legendPopulationText / 2;
-    //     return "translate(" + [x, 88] + `)`;
-    //   });
   }
 
   render() {
